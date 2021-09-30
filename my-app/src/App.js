5ec41b707e1f3bcbe9b4700e2d18c92b2299220b
Johan Ussa <johanuss0405@gmail.com>
2021-09-29 20:24:24 -0500

    Aprendiendo a manejar React

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/header";
+import Dish from "./components/dish";
+import './styles/App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dish />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/dish.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dish() {
+    return(
+        <div>
+            <h1>Aprendiendo React</h1>
+        </div>
+    )    
+}
+
+export default Dish;
